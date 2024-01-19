f6c8fb1b3e107c030b3608ca1a45115ded4cb066
debabin <dmitriybabinup@gmail.com>
2024-01-20 01:57:01 +0700

    🎉 init

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -19,15 +19,15 @@ export const RootPage = () => {
     <>
       <h1>Pokemon first gen</h1>
       <div className='pokemons_container'>
-        {list.map((character, index) => (
-          <div key={character.name} className={'pokemon'}>
+        {list.map((pokemon, index) => (
+          <div key={pokemon.name} className={'pokemon'}>
             <img
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
                 index + 1
               }.png`}
-              alt='character'
+              alt={pokemon.name}
             />
-            <div>{character.name}</div>
+            <div>{pokemon.name}</div>
           </div>
         ))}
       </div>
